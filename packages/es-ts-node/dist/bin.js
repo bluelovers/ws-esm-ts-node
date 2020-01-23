86e0d6c2b75866412d8a83c58d575b57e5f2a0a3
bluelovers <codelovers@users.sourceforge.net>
2020-01-23 22:48:43 +0800

    .

---
+++ packages/es-ts-node/dist/bin.js
@@ -0,0 +1,10 @@
+#!/usr/bin/env node
+var __importStar = (this && this.__importStar) || function (mod) {
+    if (mod && mod.__esModule) return mod;
+    var result = {};
+    if (mod != null) for (var k in mod) if (Object.hasOwnProperty.call(mod, k)) result[k] = mod[k];
+    result["default"] = mod;
+    return result;
+};
+Promise.resolve().then(() => __importStar(require('../bin')));
+//# sourceMappingURL=data:application/json;base64,eyJ2ZXJzaW9uIjozLCJmaWxlIjoiYmluLmpzIiwic291cmNlUm9vdCI6IiIsInNvdXJjZXMiOlsiYmluLnRzIl0sIm5hbWVzIjpbXSwibWFwcGluZ3MiOiI7Ozs7Ozs7O0FBRUEsa0RBQU8sUUFBUSxJQUFFIiwic291cmNlc0NvbnRlbnQiOlsiIyEvdXNyL2Jpbi9lbnYgbm9kZVxuXG5pbXBvcnQoJy4uL2JpbicpO1xuIl19
